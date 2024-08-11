dd8ca9f3b65d27d44cf39f0d58516eb675461c78
Trunicz <romelelicg_19@alu.uabcs.mx>
2024-08-11 07:43:47 -0700

    SEO: se agrego la posibilidad de que el sitemap se creara automaticamente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://choya.tech',
   integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }),]
+    applyBaseStyles: false
+  }), sitemap()]
 });
