99e29a220671bc237e210b7dc1b4d8125d5de78e
Trunicz <romelelicg_19@alu.uabcs.mx>
2024-08-12 12:50:52 -0700

    Install dependecies

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://choya.tech',
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), sitemap()]
+  integrations: [react(), tailwind(), sitemap()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -47,5 +46,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  darkMode: "class",
+  plugins: [],
 }
